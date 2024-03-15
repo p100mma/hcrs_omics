507ebe0c59a00069387df196ec66e66181c615ea
Piotr Stomma <p.stomma@uwb.edu.pl>
2024-03-15 17:21:45 +0100

    removed one variant exclued from paper

--- computation_heavy_metrics.R
+++ computation_heavy_metrics.R
@@ -22,12 +22,6 @@ colnames(SVD_res10$X_rec)<- colnames(X)
 colnames(SVD_res10$X_rec_noise)<- colnames(X)
 
 
-readRDS( 'plainSVDreconstruct48perc.rds')->SVD_res48p
-# @ UP : list(PC=PC,
-#	      X_rec=X_rec,
-#	      X_rec_noise=X_rec_noise), 
-colnames(SVD_res48p$X_rec)<- colnames(X)
-colnames(SVD_res48p$X_rec_noise)<- colnames(X)
 
 X_tables_mcl<- list(
                 rec=reclist$X_rec,
@@ -46,12 +40,10 @@ X_tables<- c( list( orig=X,
 		    WGCNA=WGCNA_tab,
 		    SVD_70=SVD_res70$X_rec, 
 		    SVD_10=SVD_res10$X_rec, 
-		    SVD_48p=SVD_res48p$X_rec 
 		  ),
 	      X_tables_mcl[1:3],
 	     list( SVD_70_noise=SVD_res70$X_rec_noise,
 	           SVD_10_noise=SVD_res10$X_rec_noise,
-	           SVD_48p_noise=SVD_res48p$X_rec_noise
 		),
 	      X_tables_mcl[4:6]
 	    )
